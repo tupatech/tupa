c91a0e8d9348dc14826ae613b7c3b0d84a9094c8
victor <victor@iuptec.com.br>
2024-03-27 14:58:40 -0300

    feat: add campos googleDefaultResponse

--- auth.go
+++ auth.go
@@ -43,7 +43,11 @@ type GoogleDefaultResponse struct {
 	ID            string `json:"id"`
 	Email         string `json:"email"`
 	VerifiedEmail bool   `json:"verified_email"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
 	Picture       string `json:"picture"`
+	Locale        string `json:"locale"`
 	HostedDomain  string `json:"hd"`
 }
 
